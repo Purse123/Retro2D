b00cb8d49fd472363152b0368bc16244a5826d7d
Pierce Neupane <sahasneupane85@gmail.com>
2025-05-23 23:12:34 +0545

    gameObject created

--- Overworld.js
+++ Overworld.js
@@ -10,37 +10,24 @@ class Overworld {
 	const image = new Image();
 	image.onload = () => {
 	    this.ctx.drawImage(image, 0, 0)
-	}
+	};
 	image.src = "./images/maps/DemoLower.png";
 
-	// character
-	const x = 5;
-	const y = 6;
+	// Game object placement
+	const hero = new GameObject({
+	    x: 5,
+	    y: 6,
+	})
+	
+	const npc1 = new GameObject({
+	    x: 7,
+	    y: 9,
+	    src: "./images/characters/people/npc1.png",
+	})
 
-	// shadow
-	const shadow = new Image();
-	shadow.onload = () => {
-	    this.ctx.drawImage(
-		shadow,           
-		0, 0,           
-		32, 32,         
-		x * 16 - 8, y * 16 - 18, 
-		32, 32,
-	    )
-	}	
-	shadow.src = "./images/characters/shadow.png";
-
-	const hero = new Image();
-	hero.onload = () => {
-	    this.ctx.drawImage(
-		hero,           // name
-		0, 0,           // crop (left, top)
-		32, 32,         // width, height of cut
-		x * 16 - 8, y * 16 - 18, // position of character
-		// * 16 for grid size
-		32, 32,         // size/scale of character on map
-	    )
-	}
-	hero.src = "./images/characters/people/hero.png";
+	setTimeout(() => {
+	    hero.sprite.draw(this.ctx);
+	    npc1.sprite.draw(this.ctx);
+	}, 200)
     }
 }
